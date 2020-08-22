106249b40f29b0b1de6bbf6a3f830208b38d50a7
David Schnell-Davis <dschnelldavs@gmail.com>
2020-08-22 13:41:20 -0700

    Initial commit

---
+++ R/prediction.R
@@ -0,0 +1,19 @@
+library(survey)
+#' Prediction
+#'
+#' Bankruptcy prediction function
+#'
+#' @export
+#' @param input. Required.
+prediction <- function(inputformula, inputvalues) {
+	useformula <- as.formula(inputformula)
+	usevalues <- as.data.frame(inputvalues)
+	design34 <- svydesign(id = ~casenumber, weights = ~pweight, data = data)
+	model34 <- svyglm(lnfeeexpord ~ lnroles + lnassets + lndaysin + yearconfirmed + lnemployees + shop + saleall, design = design34)
+	newdata1 <- with(data, usevalues)
+	pred <- predict(model34, newdata = newdata1, se.fit = T)
+	efit <- pred[1]
+	lfit <- efit - 1.44 * sqrt(attr(pred, "var")) / .97
+	hfit <- efit + 1.44 * sqrt(attr(pred, "var")) / .97
+	list(efit = efit, hfit = hfit, lfit = lfit)
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+feecalculatorprediction
+==================
+
+The LoPucki-Doherty Professional Fees Calculator: Prediction
+
+Function for estimating bankruptcy professional fee and expense awards.
+
+[LoPucki-Doherty Bankruptcy Professional Fees Prediction Calculator](http://lopucki.law.ucla.edu/fee_calculator_prediction.asp)
