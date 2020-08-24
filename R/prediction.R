a2389bf225ac521446986f7839028772031611f1
David Schnell-Davis <dschnelldavs@gmail.com>
2020-08-24 11:15:34 -0700

    Change formula from 3.4 to 3.1

--- R/prediction.R
+++ R/prediction.R
@@ -8,10 +8,10 @@ prediction <- function(inputformula, inputvalues) {
 	library(survey)
 	useformula <- as.formula(inputformula)
 	usevalues <- as.data.frame(inputvalues)
-	design34 <- svydesign(id = ~casenumber, weights = ~pweight, data = predictiondata)
-	model34 <- svyglm(lnfeeexpord ~ lnroles + lnassets + lndaysin + yearconfirmed + lnemployees + shop + saleall, design = design34)
+	design31 <- svydesign(id = ~casenumber, weights = ~pweight, data = predictiondata)
+	model31 <- svyglm(lnfeeexpord ~ lnassets + prepack + preneg + lnsales + xyearfiled + shop, design = design31)
 	newdata1 <- with(predictiondata, usevalues)
-	pred <- predict(model34, newdata = newdata1, se.fit = T)
+	pred <- predict(model31, newdata = newdata1, se.fit = T)
 	efit <- pred[1]
 	lfit <- efit - 1.44 * sqrt(attr(pred, "var")) / .97
 	hfit <- efit + 1.44 * sqrt(attr(pred, "var")) / .97
